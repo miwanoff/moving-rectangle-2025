714cb66a9a8d8917ab41db8d4cda49fda3fb1619
miwanoff <marina.iva.noffa@gmail.com>
2025-02-01 15:15:02 +0200

    Add moving box

--- script.js
+++ script.js
@@ -20,9 +20,37 @@ let topOffset = 0; //  координата по Y
 let box = document.getElementById('box');
 
 function moveBox() { // рухаємо квадратик
-    leftOffset += 1;
+    leftOffset++;
     box.style.left = leftOffset + "px";
     box.style.top = topOffset + "px";
+
+    if (leftOffset > 200) { // якщо leftOffset досягла 200, то квадрат праворуч
+        right = true
+    } else if (leftOffset == 0) { // якщо leftOffset == 0, то квадрат зліва
+        right = false;
+    }
+
+    if (topOffset > 200) { // якщо topOffset досягла 200, то квадрат внизу
+        bottom = true
+    } else if (topOffset == 0) { // якщо topOffset == 0, то квадрат зверху
+        bottom = false;
+    }
+
+    if (bottom == false && right == false) { // якщо квадрат у верхньому лівому кутку, рухаємо вниз
+        topOffset += 30;
+    }
+
+    if (bottom == false && right == true) { // якщо квадрат у верхньому правому кутку, рухаємо ліворуч
+        leftOffset -= 30;
+    }
+
+    if (bottom == true && right == false) { // якщо квадрат у верхньому лівому кутку, рухаємо праворуч
+        leftOffset += 30;
+    }
+
+    if (bottom == true && right == true) { // якщо квадрат у правому лівому кутку, рухаємо вгору
+        topOffset -=  30;
+    }
 }
 
 intervalId = setInterval(moveBox, time);
